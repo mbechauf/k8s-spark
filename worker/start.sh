8fc32bc2e933b4c16d381bec42eda00ace611937
Michael Bechauf <bechauf.michael@gmail.com>
2015-07-07 12:03:34 +0000

    first commit

---
+++ worker/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# name resolution for spark-master
+echo "${SPARK_MASTER_SERVICE_HOST:-$1} spark-master" >> /etc/hosts
+
+# because the hostname only resolves locally
+export SPARK_LOCAL_HOSTNAME=$(hostname -i)
+
+/usr/share/spark/sbin/start-slave.sh 8 spark://spark-master:7077
+
+# TODO: detect slave exit
+tail -F /usr/share/spark/logs/*
